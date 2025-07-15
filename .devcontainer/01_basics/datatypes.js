317d451b5904237e3df25bb207fc82b6eb7adabc
aliyabatool-142 <aliaraza4591@gmail.com>
2025-07-15 12:27:12 +0000

    datatypes

---
+++ .devcontainer/01_basics/datatypes.js
@@ -0,0 +1,17 @@
+//alert(3+3);    //we are using not browser
+let name = "Aliya";
+let age = 21;
+let isDtudent = true;
+
+//number -> 2 to power53
+//bigint -> 2 to power 64
+//string -> "string" or 'string' or `string`
+//boolean -> true or false
+//null -> empty value (standalone value)
+//undefined -> variable is declared but not assigned
+//symbol -> unique value (ES6 feature)
+
+//object -> collection of key-value pairs
+console.log(typeof age);
+console.log(typeof null);
+console.log(typeof undefined);
