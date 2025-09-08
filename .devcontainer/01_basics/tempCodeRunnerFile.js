ce38a269c69ad47d70285383593842874dbb6971
aliyabatool-142 <aliaraza4591@gmail.com>
2025-09-08 12:15:54 +0000

    container config

--- .devcontainer/01_basics/tempCodeRunnerFile.js
+++ .devcontainer/01_basics/tempCodeRunnerFile.js
@@ -1,13 +1,1 @@
-//print 1 to 10 with delay of 1 sec after each no. gets printed
-// for(let i=1; i<=10; i++){
-//     setTimeout(function(){
-//         console.log(i);
-//     }, i*1000)
-// }
-
-// //print 10 to 1 with delay of 1 sec after each no. gets printed
-// for(let i=1; i<=10; i++){
-//     setTimeout(function(){
-//         console.log(11-i);
-//     }, i*1000)
-// }
+let arr = [1,2,-3,4,5];
